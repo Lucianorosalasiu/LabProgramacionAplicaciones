a870207a321e71bf641d67429ce85bb2659161fa
diegoVillalba5 <116387236+diegoVillalba5@users.noreply.github.com>
2023-08-19 19:23:38 -0300

    Creacion Salida Turistica
    Se crea la clase salida turistica e inscripcion.
    Se agrega la dependencia lombok 1.18.28 para el uso de antoaciones como @Getter y @Setter

---
+++ LabProgramacionAplicaciones/src/main/java/clases/Inscripcion.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clases;
+
+import java.util.Date;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * @author diego
+ */
+@Getter
+@Setter
+public class Inscripcion {
+       //Turista turista;
+       SalidaTuristica salidaTuristica;
+       Date fecha;
+       int cantidadTuristas;
+       float costoTotal;
+
+}
---
+++ LabProgramacionAplicaciones/src/main/java/clases/SalidaTuristica.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clases;
+
+import java.util.Date;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * @author diego
+ */
+
+@Getter
+@Setter
+public class SalidaTuristica {
+    private String nombre;
+    private int cantidadMaxTuristas;
+    private Date fechaSalida;
+    private String lugar;
+    private Date fechaAlta;
+    
+    
+}
